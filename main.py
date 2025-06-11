9f344705fd7ed19a8b2ac1945b54cbd517c585f1
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-06-11 19:47:45 +0600

    password now saves onto a text file

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from tkinter import *
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n ")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -12,13 +24,13 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 
-
+#canvas
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-
+#labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 email_label = Label(text="Email:")
@@ -26,7 +38,7 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-
+#inputs
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
 email_entry = Entry(width=35)
@@ -35,7 +47,7 @@ email_entry.insert(END, "emrn.hossn@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
-
+#buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36)
